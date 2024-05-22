b240d1090d6a35062e5d1dffa46e4b29fa018c56
Sunlab-UGA <sunlab.uga@gmail.com>
2024-05-22 09:37:38 -0400

    updated to note, not to use

--- python/git_revert.py
+++ python/git_revert.py
@@ -1,14 +1,5 @@
-# this is the main file that runs the full NOFEC-FEDERATED learing application
-# load the environment variables
-
-# server waits till two client models are received and saved
-# then it averages the weights* saves the model and sends it back to the clients
-# * always taking the newest model from the clients
-
-# a client trains a model, saves, and waits to transmit
-# on a sync time it transmits the model to the server
-# then it waits for a new saved model from the server
-# (if none it resends the same model at the next sync time)
+# uses git revert on both clients
+# dont use this...just use git ignore on the logs and models
 
 import subprocess
 import threading
